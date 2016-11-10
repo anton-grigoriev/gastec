b62b27f3961f7286d8fc69902e0fe2b7dd4c13f1
Anton Grigoriev <anton.grigoriev@gmail.com>
2016-11-10 08:28:54 +0000

    robots.txt generation added

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+add_action( 'do_robotstxt', 'my_robotstxt' );
+function my_robotstxt() {
+	echo 'User-agent: *' . PHP_EOL;
+	echo 'Host: gastec.su' . PHP_EOL;
+	die; 
+}
+
 //JavaScript
 function enqueue_scripts () {
     wp_register_script('html5-shiv', 'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js');
@@ -39,10 +46,12 @@ add_filter( 'show_admin_bar', '__return_false' );
 //setting up featured images
 add_theme_support( 'post-thumbnails' );
 
-//setting up top menu
-function register_my_menu() {
-  register_nav_menu('gstc-top-menu',__( 'Header Menu' ));
-}
-add_action( 'init', 'register_my_menu' );
+// setting up top menu
+// function register_my_menu() {
+//   register_nav_menu('gstc-top-menu',__( 'Header Menu' ));
+// }
+// add_action( 'init', 'register_my_menu' );
+
+
 
 ?>
